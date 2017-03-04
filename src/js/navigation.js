b556876acc4d95d7b9a5ec32c656831b249d6edd
Bozhena Bodnar <bod.bozena@gmail.com>
2017-03-04 23:07:37 +0200

    random card pick was added

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,3 +1,81 @@
-function openCard(){
+function openCard(id){
+  if(typeof(Storage)!=="undefined")
+  {
+    localStorage.title=document.getElementById(id).innerHTML;
+  }
   window.location.href="card.html";
+}
+
+function get(id){
+  if(typeof(Storage)!=="undefined" && localStorage.title)
+  {
+    window.document.getElementById('generalCardInfo').innerHTML = localStorage.title;
+    alert(title);
+    document.getElementById(id).innerHTML = localStorage.title;
+  }
+}
+
+var category_1 = [
+    'Tree of Wishes',
+    'Tree of Wishes 1',
+    'Tree of Wishes 2'
+];
+
+
+var category_2 = [
+    'Smile and Count',
+    'Smile and Count 1',
+    'Smile and Count 2'
+];
+
+
+var category_3 = [
+    'Paper Bowl',
+    'Paper Bowl 1',
+    'Paper Bowl 2'
+];
+
+var category_4 = [
+    'Funny Seasons',
+    'Funny Seasons 1',
+    'Funny Seasons 2'
+];
+
+var category_5 = [
+    'Christmas Baal with',
+    'Christmas Baal with 1',
+    'Christmas Baal with 2'
+];
+
+var category_6 = [
+    'Thank you Card',
+    'Thank you Card 1',
+    'Thank you Card 2'
+];
+
+var category_7 = [
+    'Happy Mother\'s Day',
+    'Happy Mother\'s Day 1',
+    'Happy Mother\'s Day 2'
+];
+
+var category_8 = [
+    '1st April. Day of Fun!',
+    '1st April. Day of Fun! 1',
+    '1st April. Day of Fun! 2'
+];
+
+function getRandomCard(category){
+  return Math.floor(Math.random()*category.length);
+}
+function setTitle(){
+  document.getElementById("card_1").innerHTML=category_1[getRandomCard(category_1)];
+  document.getElementById("card_2").innerHTML=category_2[getRandomCard(category_2)];
+  document.getElementById("card_3").innerHTML=category_3[getRandomCard(category_3)];
+  document.getElementById("card_4").innerHTML=category_4[getRandomCard(category_4)];
+  document.getElementById("card_5").innerHTML=category_5[getRandomCard(category_5)];
+  document.getElementById("card_6").innerHTML=category_6[getRandomCard(category_6)];
+  document.getElementById("card_7").innerHTML=category_7[getRandomCard(category_7)];
+  document.getElementById("card_8").innerHTML=category_8[getRandomCard(category_8)];
+
 }
