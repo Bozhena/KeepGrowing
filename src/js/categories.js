9d4c6a02c443ad1cd1456ef5caa2b403abda84b5
Denis Yermakov <dan@rebbix.com>
2017-07-30 17:59:52 +0300

    Refactoring CSS

---
+++ src/js/categories.js
@@ -0,0 +1,53 @@
+var category_1 = [
+  'Tree of Wishes',
+  'Tree of Wishes 1',
+  'Tree of Wishes 2'
+];
+
+var category_2 = [
+  'Smile and Count',
+  'Smile and Count 1',
+  'Smile and Count 2'
+];
+
+var category_3 = [
+  'Paper Bowl',
+  'Paper Bowl 1',
+  'Paper Bowl 2'
+];
+
+var category_4 = [
+  'Funny Seasons',
+  'Funny Seasons 1',
+  'Funny Seasons 2'
+];
+
+var category_5 = [
+  'Christmas Baal with',
+  'Christmas Baal with 1',
+  'Christmas Baal with 2'
+];
+
+var category_6 = [
+  'Thank you Card',
+  'Thank you Card 1',
+  'Thank you Card 2'
+];
+
+var category_7 = [
+  'Happy Mother\'s Day',
+  'Happy Mother\'s Day 1',
+  'Happy Mother\'s Day 2'
+];
+
+var category_8 = [
+  '1st April. Day of Fun!',
+  '1st April. Day of Fun! 1',
+  '1st April. Day of Fun! 2'
+];
+
+var category_9 = [
+  '1st April. Day of Fun!',
+  '1st April. Day of Fun! 1',
+  '1st April. Day of Fun! 2'
+];
--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,36 @@
 function onPageLoad() {
   // init();
-  // setTitles();
+  setTitles();
 }
 
-function search(e, id){
-     if(e.keyCode === 13){
-        e.preventDefault(); // Ensure it is only this code that rusn
-        localStorage.search_request = document.getElementById(id).value;
-        var res = searching(localStorage.search_request);
-        window.location.href="searchResults.html";
-     }
+function search(e, elem) {
+  if (e.keyCode === 13) {
+    e.preventDefault(); // Ensure it is only this code that run
+    localStorage.search_request = elem.value;
+    searching(localStorage.search_request);
+    window.location.href = "searchResults.html";
+  }
 }
 
-function collapseSearchField(id){
-  // document.getElementById(id).style.backgroundImage = "url('../images/Search-with-dots.webp')";
-  var elem = document.getElementById(id);
-  elem.className = "collapsed";
+function collapseSearchField(elem) {
   elem.value = "";
+  elem.className = "collapsed";
 }
 
-function expandSearchField(id){
-  var elem = document.getElementById(id);
+function expandSearchField(elem) {
   elem.className = "expanded";
-  // elem.style.backgroundImage = "url('..//images/Search Tool.webp')";
 }
 
-function disableBlur(id){
-  document.getElementById(id).style.paddingLeft = "13%";
-}
-
-function searching(search_request){
+function searching(search_request) {
   var result = [];
   var search_requests = search_request.split(" ");
   var category = [category_1, category_2, category_3, category_4, category_5, category_6, category_7, category_8, category_9]
   var j = 0;
 
-  category.forEach(function (item, index){
-    for (i=0; i<item.length; i++){
-      search_requests.forEach(function (search_item, index){
-        if (item[i].toLowerCase().includes(search_item.toLowerCase())){
+  category.forEach(function (item, index) {
+    for (i = 0; i < item.length; i++) {
+      search_requests.forEach(function (search_item, index) {
+        if (item[i].toLowerCase().includes(search_item.toLowerCase())) {
           result[j] = item[i];
           j++;
         }
@@ -46,17 +38,34 @@ function searching(search_request){
     }
   });
 
-  localStorage["result"]  = JSON.stringify(result.filter(function(elem, index, self) {
-      return index == self.indexOf(elem);
+  localStorage["result"] = JSON.stringify(result.filter(function (elem, index, self) {
+    return index === self.indexOf(elem);
   }));
 }
 
-function searchResult(){
+function searchResult() {
   document.getElementById("search_result").value = localStorage.search_request;
   var i = 1;
-  JSON.parse(localStorage["result"]).forEach(function (item){
-    document.getElementById("card_"+i).innerHTML = item;
-    document.getElementById("card_"+i).parentNode.style.display="";
+  JSON.parse(localStorage["result"]).forEach(function (item) {
+    document.getElementById("card_" + i).innerHTML = item;
+    document.getElementById("card_" + i).parentNode.style.display = "";
     i++;
   });
+}
+
+function getRandomCard(category){
+  return Math.floor(Math.random()*category.length);
+}
+
+
+function setTitles(){
+  document.getElementById("card_1").innerHTML=category_1[getRandomCard(category_1)];
+  document.getElementById("card_2").innerHTML=category_2[getRandomCard(category_2)];
+  document.getElementById("card_3").innerHTML=category_3[getRandomCard(category_3)];
+  document.getElementById("card_4").innerHTML=category_4[getRandomCard(category_4)];
+  document.getElementById("card_5").innerHTML=category_5[getRandomCard(category_5)];
+  document.getElementById("card_6").innerHTML=category_6[getRandomCard(category_6)];
+  document.getElementById("card_7").innerHTML=category_7[getRandomCard(category_7)];
+  document.getElementById("card_8").innerHTML=category_8[getRandomCard(category_8)];
+  // document.getElementById("card_9").innerHTML=category_9[getRandomCard(category_9)];
 }
