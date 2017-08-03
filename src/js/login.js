7aafd1c873e1827691c23e2d2e19867bbb75bb28
Bozhena Bodnar <bod.bozena@gmail.com>
2017-08-03 20:57:41 +0300

    AJAX was added to login form

---
+++ src/js/login.js
@@ -0,0 +1,67 @@
+$('document').ready(function()
+{
+    /* validation */
+    $("#login-form").validate({
+        rules:
+        {
+            user_name: {
+                required: true,
+                minlength: 3
+            },
+            password: {
+                required: true,
+                minlength: 6,
+                maxlength: 15
+            },
+        },
+        messages:
+        {
+            user_name: "Enter a Valid Username",
+            password:{
+                required: "Provide a Password",
+                minlength: "Password Needs To Be Minimum of 6 Characters"
+            }
+        },
+        submitHandler: submitForm
+    });
+    /* validation */
+
+    /* form submit */
+    function submitForm()
+    {
+        var data = $("#login-form").serialize();
+
+        $.ajax({
+            type : 'POST',
+            url  : '../php/login.php',
+            data : data,
+            beforeSend: function()
+            {
+                $("#error").fadeOut(); 
+            },
+            success :  function(data)
+            {
+                if(data==1){
+
+                    $("#error").fadeIn(1000, function(){
+                        $("#error").html('<br> Invalid user name or password');
+                    });
+
+                }
+                else if(data=="exist")
+                {
+					setTimeout('$(".popup").fadeOut(500, function(){ $("#signIn").html("Add Card"); $("#signIn").attr({"id":"add_card", "onclick":"createCard()"}); $("#overlay").css("display", "none")});');
+                }
+                else{
+
+                    $("#error").fadeIn(1000, function(){
+                        $("#error").html('<br>'+data);
+                    });
+                }
+            }
+        });
+        return false;
+    }
+    /* form submit	*/
+
+});
